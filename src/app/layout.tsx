6d8328959c1abdb79ececdde600a113cbb87469b
ejjays <christsonalloso021@gmail.com>
2025-05-11 14:35:09 +0800

    EXP fixing not FS on TWA ap

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -47,6 +47,7 @@ export default function RootLayout({
            name="viewport" 
             content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, viewport-fit=cover"
          />
+         <meta name="theme-color" content="#000000">
   <link rel="manifest" href="/manifest.json" />
   <script
     dangerouslySetInnerHTML={{
